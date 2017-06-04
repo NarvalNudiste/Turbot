7c297253318b8519a74fd2cb8a27516cd26a68d6
Guillaume Noguera <Guillaume Noguera>
2017-06-04 16:23:42 +0200

    Fixed queuing

--- turbot.py
+++ turbot.py
@@ -40,13 +40,12 @@ ytdl_opts = {
 class Song:
     songCount = 0
     #song with title and location
-    def __init__(self, _name, _id, _yt):
-        self.name = _name
-        self.id = 'https://www.youtube.com/watch?v=' + _id
-        self.isYoutubeVideo = _yt
+    def __init__(self, name, url):
+        self.name = name
+        self.url = 'https://www.youtube.com/watch?v=' + url
         Song.songCount += 1
     def __str__(self):
-        return 'Song : ' + self.name + ' - Id : ' + self.id
+        return f'Song : {self.name} - url : {self.url}'
 
 with open(basePath + '\conf.json') as data:
     config = json.load(data)
@@ -76,33 +75,44 @@ async def on_ready():
        print("Error : opus module couldn't be loaded")
     print('------')
 
+async def watch(player, songId, bArgs):
+    while not player.is_done():
+            await asyncio.sleep(1)
+    if len(songs) > songId + 1:
+        songId += 1
+        ytplayer = await voice.create_ytdl_player(songs[songId].id, ytdl_options = ytdl_opts, before_options = bArgs)
+        await watch(ytplayer, songId, bArgs)
+
 #defining commands
 @client.event
 async def on_message(message):
     if message.content.startswith(yuna):
         print('command - ', message.content[1:])
         if message.content.startswith(yuna + 'queue'):
-            videoId = ydl.ytSearch(message.content[6:])
+            videoUrl = ydl.ytSearch(message.content[6:])
             global songs
-            songs.append(Song(message.content[6:], videoId, True))
+            songs.append(Song(message.content[6:], videoUrl))
             print(songs[len(songs)-1])
         elif message.content[1:] == 'join':
             try:
                 global voice
                 global ytplayer
                 global currentSongId
-
                 voice = await client.join_voice_channel(message.author.voice_channel)
-                print("current song : " + str(currentSongId))
-                print("url : " + songs[currentSongId].id)
-                beforeArgs = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
-                #songs[currentSongId].id
-                ytplayer = await voice.create_ytdl_player(songs[currentSongId].id, ytdl_options = ytdl_opts, before_options = beforeArgs)
-                ytplayer.start()
             except:
                 print(ytplayer.error)
                 pass
-        elif message.content[1:] == 'titsorgtfo':
+        elif message.content[1:] == 'start':
+            if voice is None:
+                voice = await client.join_voice_channel(message.author.voice_channel)
+            while songs:
+                currentSong = songs.pop(0)
+                beforeArgs = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
+                ytplayer = await voice.create_ytdl_player(currentSong.url, ytdl_options = ytdl_opts, before_options = beforeArgs)
+                ytplayer.start()
+                while not ytplayer.is_done():
+                    await asyncio.sleep(1)
+        elif message.content[1:] == 'gtfo':
             await voice.disconnect()
         elif message.content[1:] == 'stop':
             ytplayer.pause()
@@ -110,13 +120,9 @@ async def on_message(message):
             await client.send_message(message.channel, 'Current queue :')
             for i in range(len(songs)):
                 await client.send_message(message.channel, str(i) +  ':' + str(songs[i]))
-        elif message.content[1:] == 'setCurrentTrack':
-            target = message.content[15:]
-            print(target)
-            if target > 0 and target < len(songs):
-                currentSongId = target
         elif message.content[1:] == 'quit':
             exit()
+
 
 #running
 client.run(config['token'])

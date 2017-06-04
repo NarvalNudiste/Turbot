682a44c1affb1a222ccf8590ba81438fa241e6f4
Guillaume Noguera <Guillaume Noguera>
2017-06-04 23:00:41 +0200

    some fixes, skip is still broken tho

--- turbot.py
+++ turbot.py
@@ -1,3 +1,12 @@
+"""
+.. module:: turbot
+   :platform: Windows, I guess
+   :synopsis: simple music bot for discord
+
+.. moduleauthor:: Narval Nudiste <guillaume.noguera@gmail.com>
+
+"""
+
 from __future__ import unicode_literals
 import youtube_dl
 import os
@@ -10,6 +19,7 @@ from apiclient.discovery import build
 from apiclient.errors import HttpError
 from oauth2client.tools import argparser
 import ydl
+import random
 import os
 
 # global names
@@ -18,21 +28,19 @@ ytplayer = None
 voice = None
 songs = []
 currentSongId = 0
+skipRequestB = False
 
 # const
 ytdl_opts = {
-    'source_address': '0.0.0.0',
     'format': 'bestaudio/best',
     'extractaudio': True,
     'audioformat': "mp3",
     'outtmpl': '%(id)s',
-    'noplaylist': False,
-    'yesplaylist': True,
+    'noplaylist': True,
     'nocheckcertificate': True,
-    'ignoreerrors': True,
-    'quiet': True,
-    'no_warnings': True,
-    'outtmpl': "data/audio/cache/%(id)s",
+    'ignoreerrors': False,
+    'quiet': False,
+    'no_warnings': False,
     'default_search': 'auto'
     }
 
@@ -44,8 +52,14 @@ class Song:
         self.name = name
         self.url = 'https://www.youtube.com/watch?v=' + url
         Song.songCount += 1
+        self.infos = getVidInfo(self.url)
+        print(f"info >> {self.infos['title']}")
+
     def __str__(self):
-        return f'Song : {self.name} - url : {self.url}'
+        duration = self.infos['duration']
+        seconds = duration % 60
+        minutes = round(duration / 60)
+        return f"```{self.infos['title']} ({minutes}:{seconds})``` \b {self.url}"
 
 with open(basePath + '\conf.json') as data:
     config = json.load(data)
@@ -62,6 +76,10 @@ client = discord.Client()
 #onstart
 @client.event
 async def on_ready():
+    """Pretty self-explanatory: prints turbot ID and loads the opus module
+
+    :returns: nothing
+    """
     print(client.user.name)
     print(client.user.id)
     print('------')
@@ -75,25 +93,98 @@ async def on_ready():
        print("Error : opus module couldn't be loaded")
     print('------')
 
-async def watch(player, songId, bArgs):
-    while not player.is_done():
-            await asyncio.sleep(1)
-    if len(songs) > songId + 1:
-        songId += 1
-        ytplayer = await voice.create_ytdl_player(songs[songId].id, ytdl_options = ytdl_opts, before_options = bArgs)
-        await watch(ytplayer, songId, bArgs)
+async def pause(player):
+    """Pauses the current song
+    :param player: the ytdl_player currently playing
+    :type player: ytdl player from discord wrapper
+    :returns: nothing
+    """
+    if player.is_playing():
+        player.pause()
+
+async def resume(player):
+    """Resumes the current song
+
+    :param player: the ytdl_player currently playing
+    :type player: ytdl player from discord wrapper
+    :returns: nothing
+    """
+    if not player.is_playing():
+        player.resume()
+
+def skip_request(skipRequested):
+    """Utility func that should help to skip the current songs but actually does retard stuff and get called two times
+
+    :param skipRequested: the flag used to skip stuff
+    :type skipRequested: bool
+    :returns: bool
+    """
+    if skipRequested is True:
+        skipRequested = False
+        return True
+    else:
+        return False
+
+def getVidInfo(url):
+    """func used to get youtube information; uses youtube_dl library
+
+    :param url: the video url
+    :type url: string
+    :returns: the infos
+    """
+    yt = youtube_dl.YoutubeDL(ytdl_opts)
+    infos = yt.extract_info(url, download=False, process=False)
+    return infos
+
+async def printHelp(client, message):
+    """func used to print the help menu
+
+    :param client: this bot
+    :type client: discord.Client
+    :param message: the message that just got posted in this sweet discord chan
+    :type message: discord.Message
+    :returns: nothing
+    """
+    m = f"""```############################################################################# \b
+    # Welcome to the 1.0 turbot version
+    # commands :
+    # {yuna}queue *args* : queues a song to be played next (first youtube result) \b
+    # {yuna}join : asks the bot to join the current vocal channel \b
+    # {yuna}start : start to play the queue \b
+    # {yuna}stop : stop playback process \b
+    # {yuna}pause : pauses the playback process (wow) \b
+    # {yuna}resume : resumes the playback process (much wow) \b
+    # {yuna}viewqueue : display the current queue \b
+    # {yuna}quit : kills the bot \b
+    # {yuna}skip : skips the song, still broken tho \b
+    # {yuna}gtfo : asks the bot to kindly leaves the channel \b
+    # {yuna}emptyqueue : erases the current queue \b
+    #############################################################################````"""
+    await client.send_message(message.channel, m)
 
 #defining commands
 @client.event
 async def on_message(message):
+    """func used to process bot requests
+
+    :param message: the message that just got posted in this sweet discord chan
+    :type message: discord.Message
+    :returns: nothing
+    """
     if message.content.startswith(yuna):
-        print('command - ', message.content[1:])
+        command = message.content[1:]
+        print("command received : " + command)
         if message.content.startswith(yuna + 'queue'):
+            #parses the request to find a video link, then add a song object with the url in it
             videoUrl = ydl.ytSearch(message.content[6:])
             global songs
-            songs.append(Song(message.content[6:], videoUrl))
+            if random.randrange(10000) <= 1:
+                #most needed feature
+                songs.append(Song('[T]//','oHg5SJYRHA0'))
+            else:
+                songs.append(Song(message.content[6:], videoUrl))
             print(songs[len(songs)-1])
-        elif message.content[1:] == 'join':
+        elif command == 'join':
             try:
                 global voice
                 global ytplayer
@@ -102,27 +193,46 @@ async def on_message(message):
             except:
                 print(ytplayer.error)
                 pass
-        elif message.content[1:] == 'start':
+        elif command == 'start':
             if voice is None:
                 voice = await client.join_voice_channel(message.author.voice_channel)
+            if len(songs) == 0:
+                await client.send_message(message.channel, "```Error : ensure to queue something up before asking me to do stuff```")
             while songs:
                 currentSong = songs.pop(0)
                 beforeArgs = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
                 ytplayer = await voice.create_ytdl_player(currentSong.url, ytdl_options = ytdl_opts, before_options = beforeArgs)
                 ytplayer.start()
+                await client.send_message(message.channel, f"```Currently playing : ```{str(currentSong)}")
                 while not ytplayer.is_done():
-                    await asyncio.sleep(1)
-        elif message.content[1:] == 'gtfo':
+                    global skipRequestB
+                    if skip_request(skipRequestB):
+                        ytplayer.stop()
+                        await asyncio.sleep(3)
+                    else:
+                        await asyncio.sleep(1)
+        elif command == 'skip':
+            skipRequestB = True
+        elif command == 'pause':
+            await pause(ytplayer)
+        elif command == 'resume':
+            await resume(ytplayer)
+        elif command == 'gtfo':
             await voice.disconnect()
-        elif message.content[1:] == 'stop':
+        elif command == 'stop':
             ytplayer.pause()
-        elif message.content[1:] == 'viewQueue':
-            await client.send_message(message.channel, 'Current queue :')
+        elif command == 'viewqueue':
+            m = f'```Current queue : ({len(songs)} songs \b)'
             for i in range(len(songs)):
-                await client.send_message(message.channel, str(i) +  ':' + str(songs[i]))
-        elif message.content[1:] == 'quit':
+                m += (f"{i}: {songs[i].infos['title']} \b")
+            m += ('```')
+            await client.send_message(message.channel, m)
+        elif command == 'quit':
             exit()
-
+        elif command == 'help':
+            await printHelp(client, message)
+        elif command == 'emptyqueue':
+            songs = []
 
 #running
 client.run(config['token'])
